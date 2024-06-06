fc7fa17626aff86f04decf1a45c6fa70d502528a
Hannah Overton <hannahoverton99@gmail.com>
2024-06-06 10:50:25 -0500

    Task 2 complete

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -44,6 +44,18 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        List<Job>jobs=new List<Job>();
+         if (value.ToLower().Equals("view all"))
+        {
+            jobs=JobData.FindAll();
+            ViewBag.title="All Jobs";
+        }
+        else 
+        {
+            jobs=JobData.FindByColumnAndValue(column, value);
+            ViewBag.title= "Jobs by " + ColumnChoices[column] + ": " + value;
+        }
+        ViewBag.jobs=jobs;
         return View();
     }
 }
