213602e7161b15b99a9578ecafa2aac40aeca022
Hannah Overton <hannahoverton99@gmail.com>
2024-06-07 11:22:20 -0500

    Task 3 complete tests 2 and 3 passing

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,28 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+   
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+            List<Job>jobs=new List<Job>();
+        if(string.IsNullOrEmpty(searchTerm))
+        {
+            jobs = JobData.FindAll();
+            ViewBag.title="All Jobs";
+        }
+        else if (searchTerm.ToLower().Equals("all"))
+        {
+            jobs=JobData.FindAll();
+            ViewBag.title="All Jobs";
+        }
+        else
+        {
+            jobs=JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.title= "Jobs by " + searchType + ": " + searchTerm;
+        }
+        ViewBag.jobs=jobs;
+        ViewBag.columns= ListController.ColumnChoices;
+        return View("~/Views/List/Jobs.cshtml");
+    }
 }
 
